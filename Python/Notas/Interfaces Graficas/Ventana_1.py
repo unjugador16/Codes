f3979d3693e92c7af822b142554af7599c2d97ed
Tomas Bermudez <bermudeztomas06@gmail.com>
2023-10-24 21:04:56 -0500

    Labels :D

--- Python/Notas/Interfaces Graficas/Ventana_1.py
+++ Python/Notas/Interfaces Graficas/Ventana_1.py
@@ -6,8 +6,18 @@ ventana.geometry('500x300') # tamaño en ancho x alto, pero se puede redimension
 ventana.config(bg='lightblue')  # color de fondo
 ventana.title('Ventana Principal') # el nombre de la ventana
 ventana.resizable(width=False, height=False) # se bloquea la redimension manual
-
+ventana.iconbitmap('Python\\Notas\\Interfaces Graficas\\python.ico') # se le pasa la ruta del icono de la ventana
+ 
 ventana.mainloop() # esto es un metodo de la clase que hace que la ventana esté todo el tiempo activa
 
-#========================================= Ventana Label =========================================#
+#============================================ Labels =============================================#
 
+ventana = Tk()
+ventana.geometry('512x712')
+
+etiqueta = Label(ventana, text ='Hola Mundo', fg ='blue', font =('Cascadia code',18)) # texto que aparece en la ventana y se colora de azul, la fuente es Cascadia code y el tamaño es 18
+etiqueta.place(x =200, y =100) # posicion del texto en la ventana, se separa de izquiera a derecha y de arriba a abajo
+image = PhotoImage(file='Python\\Notas\\Interfaces Graficas\\holamundo.png') # se crea un objeto de la clase PhotoImage y se le pasa la ruta de la imagen
+etiqueta2 = Label(ventana, image = image).place(y =200) # se crea una etiqueta para mostrar la imgaen y se le pasa la imagen
+
+ventana.mainloop()
